7fc09b7ea70a6484d09bf9684f0960972a06c892
RichardPickman <drezzerock@gmail.com>
2026-02-28 14:49:16 +0100

    add useTyping hook to handle entered input as well as amount of keystrokes

---
+++ src/shared/hooks/useTyping.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export const useTyping = (sentence: string) => {
+    const [input, setInput] = useState("");
+    const [startTime, setStartTime] = useState<number | null>(null);
+    const [totalKeystrokes, setTotalKeystrokes] = useState(0);
+
+    const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const val = e.target.value;
+
+        if (!startTime && val.length > 0) {
+            setStartTime(Date.now());
+        }
+
+        if (val.length <= sentence.length) {
+            setInput(val);
+            setTotalKeystrokes((prev) => prev + 1);
+        }
+    };
+
+    return { input, onInputChange, totalKeystrokes, reset: () => setInput("") };
+};
