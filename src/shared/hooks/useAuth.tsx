f1d059877b95fa6294f293e0ef70a9e948755450
RichardPickman <drezzerock@gmail.com>
2026-02-28 14:47:09 +0100

    add useAuth hook to handle user authentication

---
+++ src/shared/hooks/useAuth.tsx
@@ -0,0 +1,30 @@
+import { useUser } from "@/entities/User";
+import { useEffect, useState } from "react";
+import { socket } from "../../../socket";
+
+export const useUserAuth = () => {
+    const { user, setUser } = useUser();
+    const [isLoggingIn, setIsLoggingIn] = useState(false);
+
+    const login = (username: string) => {
+        setIsLoggingIn(true);
+        socket.emit("join-game", username);
+    };
+
+    useEffect(() => {
+        if (!socket) return;
+
+        socket.on("login-success", (userData) => {
+            setUser(userData);
+            setIsLoggingIn(false);
+
+            localStorage.setItem("typer_username", userData.name);
+        });
+
+        return () => {
+            socket.off("login-success");
+        };
+    }, [setUser]);
+
+    return { user, login, isLoggingIn };
+};
