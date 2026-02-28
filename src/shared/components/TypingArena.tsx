bdd7cc6e3cfaab3906a16bf30cca49525ec53482
RichardPickman <drezzerock@gmail.com>
2026-02-28 15:04:05 +0100

    add typing arena component which handles whole writing process

---
+++ src/shared/components/TypingArena.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { useUser } from "@/entities/User";
+import { useGameState } from "../hooks/useGameState";
+import { useTyping } from "../hooks/useTyping";
+import { TypingDisplay } from "./TypingDisplay";
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardHeader,
+    CardTitle,
+} from "./ui/card";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { Progress } from "./ui/progress";
+
+export const TypingArena = () => {
+    const { user } = useUser();
+    const { sentence, timer, phase } = useGameState();
+    const { input, onInputChange } = useTyping(sentence);
+
+    if (!user) {
+        return null;
+    }
+
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>
+                    {phase === "LOBBY" && (
+                        <Label className="text-orange-200">
+                            Waiting for game to start...
+                        </Label>
+                    )}
+                    {phase === "RACING" && (
+                        <Label className="text-red-500">GAME STARTED</Label>
+                    )}
+                </CardTitle>
+                <CardAction className="text-red-500">
+                    {timer} SECONDS LEFT
+                </CardAction>
+            </CardHeader>
+            <CardContent>
+                <div className="space-y-4">
+                    <TypingDisplay sentence={sentence} userInput={input} />
+                    <Input
+                        onChange={(e) => onInputChange(e.currentTarget.value)}
+                        value={input}
+                        disabled={phase !== "RACING"}
+                    />
+                    <Progress value={user.progress} />
+                </div>
+            </CardContent>
+        </Card>
+    );
+};
